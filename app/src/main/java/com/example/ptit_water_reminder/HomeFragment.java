3ac3b059951da8126b22a2217197e0629f1a1903
1997808 <khanhhuyle22@gmail.com>
2021-12-18 21:23:53 +0700

    new progress circle

--- app/src/main/java/com/example/ptit_water_reminder/HomeFragment.java
+++ app/src/main/java/com/example/ptit_water_reminder/HomeFragment.java
@@ -8,6 +8,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.progressindicator.CircularProgressIndicator;
+
 public class HomeFragment extends Fragment {
     public HomeFragment() {
         // Required empty public constructor
@@ -26,6 +28,9 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        CircularProgressIndicator progress = view.findViewById(R.id.progress_circular_waterlog);
+        progress.setProgressCompat(30, true);
+        return view;
     }
 }
